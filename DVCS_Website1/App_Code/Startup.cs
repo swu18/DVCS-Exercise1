2117a5b673a30bf48a70088354f84f1b70a68116
unknown <swu18@W303A-04.nait.ca>
2015-09-25 14:34:56 -0600

    DVCS_Exercise1
    DVCS_Exercise1

---
+++ DVCS_Website1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVCS_Website1.Startup))]
+namespace DVCS_Website1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
